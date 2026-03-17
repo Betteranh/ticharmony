8b063644164b6af07190fd7d9a033eb91ebf3c5b
Betteranh <danguyen.1129@gmail.com>
2026-03-17 16:30:10 +0100

    Maj du dashboad client

--- src/main/java/be/stage/ticharmony/config/NotificationAdvice.java
+++ src/main/java/be/stage/ticharmony/config/NotificationAdvice.java
@@ -4,6 +4,7 @@ import be.stage.ticharmony.model.Notification;
 import be.stage.ticharmony.model.User;
 import be.stage.ticharmony.service.NotificationService;
 import be.stage.ticharmony.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,6 +19,25 @@ public class NotificationAdvice {
     private NotificationService notificationService;
     @Autowired
     private UserService userService;
+
+    @ModelAttribute("currentUri")
+    public String populateCurrentUri(HttpServletRequest request) {
+        return request.getRequestURI();
+    }
+
+    @ModelAttribute("currentUser")
+    public User populateCurrentUser(Authentication authentication) {
+        if (authentication == null
+                || authentication instanceof AnonymousAuthenticationToken
+                || !authentication.isAuthenticated()) {
+            return null;
+        }
+        String username = authentication.getName();
+        if (username == null || "anonymousUser".equals(username)) {
+            return null;
+        }
+        return userService.findByLogin(username);
+    }
 
     @ModelAttribute("notifications")
     public List<Notification> populateNotifications(Authentication authentication) {
